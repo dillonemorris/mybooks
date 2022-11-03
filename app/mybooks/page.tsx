091d925311396c17f1b125e7b0cb8204938bbc91
Dillon <dillonmorris91@gmail.com>
2022-11-02 17:36:16 -0700

    Setup prisma client

--- app/mybooks/page.tsx
+++ app/mybooks/page.tsx
@@ -1,4 +1,19 @@
-const MyBooks = () => {
+import type { Book } from '@prisma/client'
+import prisma from '../../lib/prisma'
+
+const getBooks = async () => {
+  // TODO: Make it so we only fetch the ones with the
+  // logged in userId as the userId
+  // where: { userId: userId }
+  const books: Book[] = await prisma.book.findMany()
+  return books
+}
+
+const MyBooks = async () => {
+  const books = await getBooks()
+
+  console.log(books)
+
   return (
     <div>
       <h1 className="text-4xl font-bold underline">Hello world 🌎</h1>
